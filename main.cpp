b9bb7ca037ac6951de2c62563c437adcea3ac785
dmitriy <dima@osipo.ru>
2026-03-24 21:52:08 +0300

    И ещё изменения

--- main.cpp
+++ main.cpp
@@ -7,9 +7,14 @@ int sum(int a, b) {
 	return a + b;
 }
 
+int sub(int a, b) {
+	return a - b;
+}
+
 
 int main() {
 	cout << sum(4, 5) << "\n";
+	cout << sub(4, 5) << "\n";
 	cout << "from git he!" << endl;
 	return 0;
 }
